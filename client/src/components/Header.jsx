d88f3daf374956d258bd05fb5d9053c236de68a5
Thirumalaikumar-M <mthirumalaitk@gmail.com>
2024-02-22 20:08:47 +0530

    Add Header component

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="max-w-[1140px] m-auto p-3 flex justify-between items-center">
+      <Link to="/" className="font-bold text-xl">
+        MyBlog
+      </Link>
+      <nav className="flex gap-4">
+        <Link to="/login" className="text-gray-700 hover:text-gray-900">
+          Login
+        </Link>
+        <Link to="/register" className="text-gray-700 hover:text-gray-900">
+          Register
+        </Link>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
