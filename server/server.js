7e283a3fa692475b91aec09af10d2e12564a3682
Thirumalaikumar-M <mthirumalaitk@gmail.com>
2024-02-25 21:43:29 +0530

    Connect MongoDB

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import mongoose from "mongoose";
 const PORT = 4000;
 
 const app = express();
@@ -7,11 +8,21 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+mongoose
+  .connect(
+    "mongodb+srv://Thirumalai:fKjxJoxpKMNBmm1T@cluster0.zbfaxl3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log("Database is connected");
+      console.log(`Server is running on port: ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
 app.post("/register", (req, res) => {
   const { username, passowrd } = req.body;
   res.json({ requestData: { username, passowrd } });
-});
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
 });
