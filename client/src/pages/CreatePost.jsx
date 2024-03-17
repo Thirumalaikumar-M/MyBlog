e1659b4112adb1f5b52b5e35d845124e51b94a8f
Thirumalaikumar-M <mthirumalaitk@gmail.com>
2024-03-17 16:56:16 +0530

    Implement Create post function

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,7 +1,76 @@
-import React from "react";
+import { useState } from "react";
+import ReactQuill from "react-quill";
+import "react-Quill/dist/quill.snow.css";
+import { Navigate } from "react-router-dom";
 
-const CreatePost = () => {
-  return <div>CreatePost</div>;
+const modules = {
+  toolbar: [
+    [{ header: [1, 2, false] }],
+    ["bold", "italic", "underline", "strike", "blockquote"],
+    [
+      { list: "ordered" },
+      { list: "bullet" },
+      { indent: "-1" },
+      { indent: "+1" },
+    ],
+    ["link", "image"],
+    ["clean"],
+  ],
 };
 
+const CreatePost = () => {
+  const [title, setTitle] = useState("");
+  const [summary, setSummary] = useState("");
+  const [content, setContent] = useState("");
+  const [files, setFiles] = useState("");
+  const [redirect, setRedirect] = useState(false);
+
+  const createNewPost = async (e) => {
+    const data = new FormData();
+    data.set("title", title);
+    data.set("summary", summary);
+    data.set("content", content);
+    data.set("content", content);
+    data.set("file", files[0]);
+    e.preventDefault();
+    const response = await fetch(`http://localhost:4000/post`, {
+      method: "POST",
+      body: data,
+    });
+    if (response.ok) {
+      setRedirect(true);
+    }
+  };
+  if (redirect) {
+    return <Navigate to="/" />;
+  }
+
+  return (
+    <form onSubmit={createNewPost}>
+      <input
+        type="title"
+        placeholder="Title"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <input
+        type="summary"
+        placeholder="Summary"
+        value={summary}
+        onChange={(e) => setSummary(e.target.value)}
+      />
+      <input type="file" onChange={(e) => setFiles(e.target.files)} />
+      <ReactQuill
+        value={content}
+        onChange={(newValue) => setContent(newValue)}
+      />
+      <button
+        type="submit"
+        className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+      >
+        Create Post
+      </button>
+    </form>
+  );
+};
 export default CreatePost;
--- server/server.js
+++ server/server.js
@@ -2,9 +2,13 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import User from "./models/User.js";
+import Post from "./models/Post.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import cookieParser from "cookie-parser";
+import multer from "multer";
+const uploadMiddleware = multer({ dest: "uploads/" });
+import fs from "fs";
 
 const PORT = 4000;
 
@@ -50,7 +54,6 @@ app.post("/login", async (req, res) => {
   const userDoc = await User.findOne({ username });
   const passOk = bcrypt.compareSync(password, userDoc.password);
   if (passOk) {
-    // logged in
     jwt.sign({ username, id: userDoc._id }, secret, {}, (err, token) => {
       if (err) throw err;
       res.cookie("token", token).json({
@@ -73,4 +76,21 @@ app.get("/profile", (req, res) => {
 
 app.post("/logout", (req, res) => {
   res.cookie("token", "").json("ok");
+});
+
+app.post("/post", uploadMiddleware.single("file"), async (req, res) => {
+  const { originalname, path } = req.file;
+  const parts = originalname.split(".");
+  const ext = parts[parts.length - 1];
+  const newPath = path + "." + ext;
+  fs.renameSync(path, newPath);
+
+  const { title, summary, content } = req.body;
+  const postDoc = await Post.create({
+    title,
+    summary,
+    content,
+    cover: newPath,
+  });
+  res.json(postDoc);
 });
